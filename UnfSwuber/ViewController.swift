99fc35da0b1f5c5e36c32782ce424a1f907862df
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-04-20 11:31:06 -0400

    Initial Commit

---
+++ UnfSwuber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnfSwuber
+//
+//  Created by Michael Dimmitt on 4/20/17.
+//  Copyright © 2017 SwuberCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
